906edfa5960575267c443325a52e15caf6fe1895
Lukasz-Bilski <lukaszbilski1@gmail.com>
2019-05-01 22:06:38 +0200

    Add methods to set paths from and to chosen by user

--- src/main/java/pl/lukaszbilski/sort/controllers/MainViewController.java
+++ src/main/java/pl/lukaszbilski/sort/controllers/MainViewController.java
@@ -1,24 +1,46 @@
 package pl.lukaszbilski.sort.controllers;
 
-import javafx.beans.property.Property;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 
-public class MainViewController {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
 
   @FXML
-  Button sortButton;
+  Button sortButton ,setPathFromButton ,setPathToButton;
 
   @FXML
   ProgressBar progressBar;
 
   @FXML
   Label labelShowName;
+
+  @FXML TextField pathFromTextField, pathToTextField;
+
+  public void initialize(URL location, ResourceBundle resources) {
+    progressBar.progressProperty().addListener(new ChangeListener<Number>() {
+      public void changed(ObservableValue<? extends Number>
+                            observable, Number oldValue, Number newValue) {
+        if (newValue.intValue() == 1) {
+          labelShowName.textProperty().unbind();
+          labelShowName.setText("KONIEC");
+          progressBar.setVisible(false);
+          sortButton.setVisible(true);
+        }
+      }
+    });
+  }
 
   /**
    * This method start after "Sortuj" button will be clicked.
@@ -48,16 +70,32 @@ public class MainViewController {
     progressBar.progressProperty().bind(task.progressProperty());
     labelShowName.textProperty().bind(task.messageProperty());
     new Thread(task).start();
+  }
 
-    progressBar.progressProperty().addListener(new ChangeListener<Number>() {
-      public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-        if(newValue.intValue() == 1){
-          labelShowName.textProperty().unbind();
-          labelShowName.setText("KONIEC");
-          progressBar.setVisible(false);
-          sortButton.setVisible(true);
-        }
-      }
-    });
+  /**
+   * This method set text in pathFromTextField.
+   */
+  public void setPathFrom() {
+    pathFromTextField.setText(getDirectoryChooser().getAbsolutePath());
+  }
+
+  /**
+   * This method set text in pathToTextField.
+   */
+  public void setPathTo() {
+    pathToTextField.setText(getDirectoryChooser().getAbsolutePath());
+  }
+
+  /**
+   * Return File directory chosen by the user.
+   */
+  private File getDirectoryChooser() {
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+    File selectedDirectory = new File(System.getProperty("user.dir"));
+
+    directoryChooser.setInitialDirectory(selectedDirectory);
+    selectedDirectory = directoryChooser.showDialog(null);
+
+    return selectedDirectory;
   }
 }
