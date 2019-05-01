54458b1efb587adfc185a7037f454c3e0131a653
Lukasz-Bilski <lukaszbilski1@gmail.com>
2019-05-01 20:49:44 +0200

    Add progressbar with sample implementation

--- src/main/java/pl/lukaszbilski/sort/controllers/MainViewController.java
+++ src/main/java/pl/lukaszbilski/sort/controllers/MainViewController.java
@@ -1,5 +1,9 @@
 package pl.lukaszbilski.sort.controllers;
 
+import javafx.beans.property.Property;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -22,6 +26,38 @@ public class MainViewController {
   public void startSort() {
     sortButton.setVisible(false);
     progressBar.setVisible(true);
-    progressBar.setProgress(-0.1);
+
+    final Task task = new Task<Void>() {
+      @Override public Void call() {
+        final long max = 50;
+        for (int i = 1; i <= max; i++) {
+          if (isCancelled()) {
+            break;
+          }
+          try {
+            Thread.sleep(25);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          updateProgress(i, max);
+          updateMessage(String.valueOf(i));
+        }
+        return null;
+      }
+    };
+    progressBar.progressProperty().bind(task.progressProperty());
+    labelShowName.textProperty().bind(task.messageProperty());
+    new Thread(task).start();
+
+    progressBar.progressProperty().addListener(new ChangeListener<Number>() {
+      public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+        if(newValue.intValue() == 1){
+          labelShowName.textProperty().unbind();
+          labelShowName.setText("KONIEC");
+          progressBar.setVisible(false);
+          sortButton.setVisible(true);
+        }
+      }
+    });
   }
 }
